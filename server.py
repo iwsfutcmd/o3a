7b82f12d4ddc8c9e13bbf0c0294c504c36aab5de
Ben Yang <beyang@adobe.com>
2022-08-15 23:15:28 -0700

    switched to flask

--- server.py
+++ server.py
@@ -1,22 +1,20 @@
 #!/usr/bin/env python3
 from o3a import o3a
 
-from sanic import Sanic
-from sanic.response import html
+from flask import Flask
 
-app = Sanic()
-app.static("/static", "./static")
+app = Flask("o3a")
 
 @app.route("/")
-async def handler(request):
-    return html(o3a("en"))
+def default_handler():
+    return o3a("en")
 
 @app.route("/<tag>")
-async def handler(request, tag="en"):
+def tag_handler(tag="en"):
     try:
-        return html(o3a(tag))
+        return o3a(tag)
     except FileNotFoundError:
-        return html("NOPE")
+        return "NOPE"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
